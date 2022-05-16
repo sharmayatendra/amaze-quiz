b586d6db004d0ca16c853164c86839187c86a13e
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-05-17 02:16:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Mockman from "mockman-js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>main page</h1>
+
+      {/* <Mockman colorScheme="dark" /> */}
+    </div>
+  );
+}
+
+export default App;
