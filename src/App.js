296d4d4222b1aca547207bb311ebe4757401b3cb
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-05-18 00:56:15 +0530

    feat: categories added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import Mockman from "mockman-js";
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/Homepage/Homepage";
+import { Categories } from "./pages/Categories/Categories";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />}></Route>
-
+      <Route path="/categories" element={<Categories />}></Route>
       {/* <Mockman colorScheme="dark" /> */}
     </Routes>
   );
---
+++ src/components/Categorycard/Categorycard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Categorycard.css"
+const Categorycard = ({category}) => {
+  return (
+    <div class="card">
+      <div class="card-wrapper">
+        <div class="card-img">
+          <img src={category.img} alt="river-scene" className="category-img"/>
+        </div>
+        <div class="card-heading-wrapper">
+          <h2 class="card-main-heading">{category.categoryName}</h2>
+          <h3 class="card-sub-heading gray-text">{category.description}</h3>
+        </div>
+        <div className="quiz-heading">
+          <h3 class="card-sub-heading gray-text">Take this quiz to test yourself</h3>
+          <h3 class="card-sub-heading gray-text">5 questions</h3>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <div class="card-footer-btn category-footer-btn">
+          <button class="btn btn-primary-solid playnow-btn">Play Now</button>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export { Categorycard };
