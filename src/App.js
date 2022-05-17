6767cfdf2e482db96f8386cdd3359ee3e6351196
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-05-18 01:56:32 +0530

    feat: rules page added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Mockman from "mockman-js";
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/Homepage/Homepage";
 import { Categories } from "./pages/Categories/Categories";
+import { Rules } from "./pages/Rules/Rules";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />}></Route>
       <Route path="/categories" element={<Categories />}></Route>
+      <Route path="/rules" element={<Rules />}></Route>
       {/* <Mockman colorScheme="dark" /> */}
     </Routes>
   );
