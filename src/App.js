68c4fde2997b131a7724374b2c896fdb30da2897
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-05-20 05:34:16 +0530

    fix: suggested changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Mockman from "mockman-js";
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/Homepage/Homepage";
 import { Categories } from "./pages/Categories/Categories";
-// import { Rules } from "./pages/Rules/Rules";
 import { Quiz } from "./pages/Quiz/Quiz";
 import { Result } from "./pages/Result/Result";
 
@@ -12,10 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Homepage />}></Route>
       <Route path="/categories" element={<Categories />}></Route>
-      {/* <Route path="/rules" element={<Rules />}></Route> */}
       <Route path="/quiz/:quizId" element={<Quiz />}></Route>
       <Route path="/result" element={<Result />}></Route>
-      {/* <Mockman colorScheme="dark" /> */}
     </Routes>
   );
 }
